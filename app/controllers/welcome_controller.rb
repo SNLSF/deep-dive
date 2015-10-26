64ae29858d364ffa849e590834a3a7275b448691
SNLSF <stephanielopez.sf@gmail.com>
2015-10-25 21:20:52 -0700

    iterate through books and influecers on index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,9 @@
 class WelcomeController < ApplicationController
 
 	def index
-		@current_book = Book.find(3)
+		@current_book = Book.find(12)
 		@current_influencers = @current_book.influencers
-		p "*" * 50
-		p @current_book.title
-		p @current_book.image
-		p @current_book.date
-		p @current_book.influencers
-
+		@books = Book.all
+		@influencers = Influencer.all
 	end
 end
