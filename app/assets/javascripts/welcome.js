95ac288ce62f706808314f3515681cc7d4281676
SNLSF <stephanielopez.sf@gmail.com>
2015-11-29 09:06:04 -0800

    add show page with data and add building blocks of book carousel on index

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -6,4 +6,37 @@ $( document ).ready(function() {
       $(this).find(':nth-child(2)').addClass('hide');
     });
 
+  // Book Carousel
+  var currentSlides = [0..1];
+
+  if 
+
+
+
+
+  // var currentSlide = 0;
+
+  // $(".next").on("click", function() {
+  //   $($(".slide")[currentSlide]).hide();
+  //   nextSlide(true);
+  // })
+
+  // $(".previous").on("click", function() {
+  //   $($(".slide")[currentSlide]).hide();
+  //   nextSlide(false);
+  // })
+
+  // function nextSlide(forward) {
+  //   if (forward == true && currentSlide == $('.slide').length - 1){
+  //     currentSlide = 0;
+  //   } else if (forward == false && currentSlide == 0){
+  //     currentSlide = $('.slide').length - 1;
+  //   } else if (forward == true){
+  //     currentSlide += 1;
+  //   } else if (forward == false) {
+  //     currentSlide -= 1;
+  //   }
+  //   $($('.slide')[currentSlide]).show();
+  // };
+
 });
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,9 @@
 class BooksController < ApplicationController
+
+	def show
+		@book = Book.find(params[:id])
+		@influencers = @book.influencers
+		p @influencers
+	end
+
 end
