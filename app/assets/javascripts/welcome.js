29e65b845ed9d9d170301997bc3f0d0d1c3fa1e7
SNLSF <stephanielopez.sf@gmail.com>
2015-11-29 12:51:22 -0800

    make carousel slides work and add css for dots

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,36 +7,37 @@ $( document ).ready(function() {
     });
 
   // Book Carousel
-  var currentSlides = [0..1];
+  ////////////////
 
-  if 
+  var currentSlide = 0;
+  var slides = $('.slide');
 
+  $('.next-icon').on("click", function() {
+    $($(".slide")[currentSlide]).hide();
+    slideForward();
+  })
 
+  $('.previous-icon').on("click", function() {
+    $($(".slide")[currentSlide]).hide();
+    slideBackward();
+  })
 
+  function slideForward() {
+    if (currentSlide == $('.slide').length - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide += 1;
+    }
+    $($(".slide")[currentSlide]).css("display", "inline-block");
+  }
 
-  // var currentSlide = 0;
-
-  // $(".next").on("click", function() {
-  //   $($(".slide")[currentSlide]).hide();
-  //   nextSlide(true);
-  // })
-
-  // $(".previous").on("click", function() {
-  //   $($(".slide")[currentSlide]).hide();
-  //   nextSlide(false);
-  // })
-
-  // function nextSlide(forward) {
-  //   if (forward == true && currentSlide == $('.slide').length - 1){
-  //     currentSlide = 0;
-  //   } else if (forward == false && currentSlide == 0){
-  //     currentSlide = $('.slide').length - 1;
-  //   } else if (forward == true){
-  //     currentSlide += 1;
-  //   } else if (forward == false) {
-  //     currentSlide -= 1;
-  //   }
-  //   $($('.slide')[currentSlide]).show();
-  // };
+  function slideBackward() {
+    if (currentSlide == 0) {
+      currentSlide = slides.length - 1;
+    } else {
+      currentSlide -= 1;
+    }
+    $($(".slide")[currentSlide]).css("display", "inline-block");
+  }
 
 });
