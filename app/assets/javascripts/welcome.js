20fb7444f877bad7bb7b0652f47a14f6e807d6e1
SNLSF <stephanielopez.sf@gmail.com>
2015-11-28 16:29:27 -0800

    animate header and add bg photo to reading section

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,31 +1,9 @@
 $( document ).ready(function() {
 
-
-  var currentSlide = 0;
-
-  $(".next").on("click", function() {
-		console.log('hello');
-    $($(".slide")[currentSlide]).hide();
-    nextSlide(true);
-  })
-
-  $(".previous").on("click", function() {
-		console.log('hello');
-    $($(".book")[currentSlide]).hide();
-    nextSlide(false);
-  })
-
-  function nextSlide(canMoveForward) {
-    if (canMoveForward == true && currentSlide == $('.slide').length - 1){
-      currentSlide = 0;
-    } else if (canMoveForward == false && currentSlide == 0){
-      currentSlide = $('.slide').length - 1;
-    } else if (canMoveForward == true){
-      currentSlide += 1;
-    } else if (canMoveForward == false) {
-      currentSlide -= 1;
-    }
-    $($('.slide')[currentSlide]).show();
-  };
+  $('.influencer-grid__tile').hover(function() {
+      $(this).find(':nth-child(2)').removeClass('hide');
+    }, function() {
+      $(this).find(':nth-child(2)').addClass('hide');
+    });
 
 });
